c7bc95b2c1c64105366930d39923bdc19c4b76d6
Сергей <itkse89@cmail.com>
2023-05-13 20:31:12 +0300

    исправляет под сафари

--- source/js/modules/burger/burger.js
+++ source/js/modules/burger/burger.js
@@ -1,6 +1,8 @@
 const burgerToggle = document.getElementById('burger');
 const menuHeader = document.getElementById('menu');
 const menuLink = document.getElementById('menu-item');
+const menuClose = document.getElementById('menu-close');
+
 
 if (burgerToggle) {
   burgerToggle.addEventListener('click', function () {
@@ -12,5 +14,11 @@ if (burgerToggle) {
         document.body.classList.toggle('lock');
       }
     });
+  });
+  menuClose.addEventListener('click', function () {
+    if (menuHeader.classList.contains('is-open')) {
+      menuHeader.classList.remove('is-open');
+      document.body.classList.toggle('lock');
+    }
   });
 }
